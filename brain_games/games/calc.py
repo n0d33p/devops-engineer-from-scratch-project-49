f70de4ebc53ef4f8ddada7884d49404426fe3b3d
n0d33p <zaycpetr@mail.ru>
2025-12-05 09:21:56 +0300

    fix ruffs errors

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -2,6 +2,7 @@ import random
 
 RULES = 'What is the result of the expression?'
 
+
 def calculate(num1, num2, operator):
     if operator == '+':
         return num1 + num2
@@ -12,6 +13,7 @@ def calculate(num1, num2, operator):
     else:
         raise ValueError(f"Unknown operator: {operator}")
     
+
 def generate_round():
     num1 = random.randint(1, 50)
     num2 = random.randint(1, 50)
